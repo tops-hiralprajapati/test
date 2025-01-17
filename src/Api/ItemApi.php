078d1218c599d4b072167e693f7b5f05c831a136
hiralprajapati <hiralprajapati@topsinfosolutions.com>
2025-01-17 16:36:33 +0530

    update item change

--- src/Api/ItemApi.php
+++ src/Api/ItemApi.php
@@ -7,37 +7,37 @@ use Tops\NeweggApi\Api\BaseApi;
 class ItemApi extends BaseApi
 {
     // Endpoint URLs for report-related actions
-    protected $getItemInventoryUrl    = "/contentmgmt/item/international/inventory";
-    protected $getItemPriceUrl        = "/contentmgmt/item/international/price";
+    protected $getBatchInventoryUrl    = "/contentmgmt/item/international/inventorylist";
+    protected $getBatchPriceUrl       = "/contentmgmt/item/international/Pricelist";
 
     /**
      * Get the item details and inventory from the API.
-     * This function sends a PUT request to the `getItemInventoryUrl` with the provided request data and parameters.
+     * This function sends a POST request to the `getBatchInventoryUrl` with the provided request data and parameters.
      *
      * @param array $reqData The data to be sent with the request.
      * @param array $params Optional additional query parameters to be appended to the URL.
      * @return array The response from the API request (success or error).
-     * @author Suraj Gupta <surajgupta@topsinfosolutions.com> | 13-01-2025
-     * @link https://developer.newegg.com/newegg_marketplace_api/item_management/get_inventory/
+     * @author Suraj Gupta <surajgupta@topsinfosolutions.com> | 17-01-2025
+     * @link https://developer.newegg.com/newegg_marketplace_api/item_management/get-batch-inventory/
     */
-    public function getItemInventory($reqData = [], $params = [])
+    public function getBatchInventory($reqData = [], $params = [])
     {
-        return $this->makeRequest($this->getItemInventoryUrl, 'PUT', $reqData, $params);
+        return $this->makeRequest($this->getBatchInventoryUrl, 'POST', $reqData, $params);
     }
 
     /**
-     * Get the item details and price from the API.
+     * Get the list of item details and price from the API.
      *
-     * This function sends a PUT request to the `getItemPriceUrl` with the provided request data and parameters.
+     * This function sends a POST request to the `getBatchPriceUrl` with the provided request data and parameters.
      *
      * @param array $reqData The data to be sent with the request.
      * @param array $params Optional additional query parameters to be appended to the URL.
      * @return array The response from the API request (status information).
-     * @author Suraj Gupta <surajgupta@topsinfosolutions.com> | 13-01-2025
-     * @link https://developer.newegg.com/newegg_marketplace_api/item_management/get_price/
+     * @author Suraj Gupta <surajgupta@topsinfosolutions.com> | 16-01-2025
+     * @link https://developer.newegg.com/newegg_marketplace_api/item_management/get-batch-price/
     */
-    public function getItemPrice($reqData = [], $params = [])
+    public function getBatchPrice($reqData = [], $params = [])
     {
-        return $this->makeRequest($this->getItemPriceUrl, 'PUT', $reqData, $params);
+        return $this->makeRequest($this->getBatchPriceUrl, 'POST', $reqData, $params);
     }
 }
